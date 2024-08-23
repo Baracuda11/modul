1a7211daf2657cf9d672025a67f81399de365322
pipin <Aleksejsoronov3@gmail.com>
2024-08-23 20:33:55 +0300

    Add files via upload

---
+++ blok.py
@@ -0,0 +1,12 @@
+first = input("Введите число: ")
+second = input("Введите число: ")
+third = input("Введите число: ")
+if first == second == third:
+    print(3)
+elif second == first:
+    print(2)
+elif first == third:
+    print(2)
+
+else:
+    print(0)
